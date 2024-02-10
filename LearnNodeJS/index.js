b87b78eed868b6e17b593e40891fcb87be48629e
The-EleetCoder <jaijain1803@gmail.com>
2024-02-10 12:02:09 +0530

    creating server and routing

--- LearnNodeJS/index.js
+++ LearnNodeJS/index.js
@@ -1,1 +1,22 @@
-console.log("Hello world!");
+// importing 
+const http = require("http");
+
+// creating server
+const server = http.createServer((req,res)=>{
+
+    // routing
+    if (req.url === '/'){
+        res.end("<h1>Home Page</h1>")
+    }
+    else if (req.url === '/about'){
+        res.end("<h1>About Page</h1>");
+    }
+    else {
+        res.end("<h1>Page not found!</h1>");
+    }
+})
+
+// listening to server
+server.listen(5000,()=>{
+    console.log("server listening on port 5000")
+});
