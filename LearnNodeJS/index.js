be2a36b64d1a067080ba421c818bbb8de38f407e
The-EleetCoder <jaijain1803@gmail.com>
2024-02-11 11:07:19 +0530

    linking static (public) files

--- LearnNodeJS/index.js
+++ LearnNodeJS/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import path from 'path'; // to get the path from scratch
 
 const app = express();
+
+// to link the static files ( the public folder )
+app.use(express.static(path.join(path.resolve(), "public"))); 
 
 // setting up view engine
 app.set('view engine', 'ejs');
