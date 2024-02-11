89acd73f7b02bb78d52f249a9ec64ea4c8c3efeb
The-EleetCoder <jaijain1803@gmail.com>
2024-02-11 18:43:55 +0530

    made contact form without db

--- LearnNodeJS/index.js
+++ LearnNodeJS/index.js
@@ -3,14 +3,29 @@ import path from 'path'; // to get the path from scratch
 
 const app = express();
 
+// using middleware
 // to link the static files ( the public folder )
 app.use(express.static(path.join(path.resolve(), "public"))); 
+app.use(express.urlencoded({extended: true}));
 
 // setting up view engine
 app.set('view engine', 'ejs');
 
+const users = [];
+
 app.get('/', (req, res) => {
     res.render('index', {name: 'Jai'})
+})
+
+app.post('/contact', (req, res) => {
+    users.push({username: req.body.name, email: req.body.email, password: req.body.password})
+    res.render('success');
+})
+
+app.get('/users', (req, res) => {
+    res.json({
+        users
+    })
 })
 
 app.listen(5000,()=>{
