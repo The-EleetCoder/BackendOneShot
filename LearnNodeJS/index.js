6266ac5e4ee677fb6ba5e4ee124273bf36c46799
The-EleetCoder <jaijain1803@gmail.com>
2024-02-17 16:35:49 +0530

    DataBase Integrated to form

--- LearnNodeJS/index.js
+++ LearnNodeJS/index.js
@@ -1,33 +1,60 @@
-import express from 'express';
-import path from 'path'; // to get the path from scratch
+import express from "express";
+import path from "path"; // to get the path from scratch
+import mongoose from "mongoose";
 
 const app = express();
 
+// connecting database
+mongoose
+  .connect("mongodb://127.0.0.1:27017", {
+    dbName: "backend",
+  })
+  .then(() => console.log("database connected"))
+  .catch((e) => console.log(e));
+
+//creating schema
+const messageSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+// creating model
+const message = mongoose.model("Message", messageSchema);
+
 // using middleware
 // to link the static files ( the public folder )
-app.use(express.static(path.join(path.resolve(), "public"))); 
-app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.urlencoded({ extended: true }));
 
 // setting up view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-const users = [];
+app.get("/", (req, res) => {
+  res.render("index", { name: "Jai" });
+});
 
-app.get('/', (req, res) => {
-    res.render('index', {name: 'Jai'})
+app.post("/contact", async (req, res) => {
+
+// saving data to database 
+  await message.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  res.redirect("/success");
+});
+
+app.get("/success",(req, res) => {
+    res.render("success");
 })
 
-app.post('/contact', (req, res) => {
-    users.push({username: req.body.name, email: req.body.email, password: req.body.password})
-    res.render('success');
-})
+app.get("/users", (req, res) => {
+  res.json({
+    users,
+  });
+});
 
-app.get('/users', (req, res) => {
-    res.json({
-        users
-    })
-})
-
-app.listen(5000,()=>{
-    console.log("Server is running");
-})
+app.listen(5000, () => {
+  console.log("Server is running");
+});
